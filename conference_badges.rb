4f2e58bc1a82224eb9c75a6d3dfce2f4865e49c9
github username <github email address>
2020-11-13 10:25:19 -0800

    commit tree dafcb0259d92c30f00ef77ca738a3d90034efe3c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+names = ["Arel", "Joe", "Matt", "Fred"]
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.each_with_index.map do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |roomnumber|
+        puts roomnumber
+    end
+end
